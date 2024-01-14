a28eacfb6bd521c766b917751123d56251b33b81
Azdad Anass <a.azdad@3gcom-int.com>
2024-01-14 10:29:46 +0100

    add slave scripts

--- database/slave/init.sh
+++ database/slave/init.sh
@@ -30,12 +30,19 @@ sudo mkdir /var/lib/mysql/mysql -p
 sudo systemctl start mysql
 
 
-read -p "Enter slave address [192.168.1.101] : " slave_ip
-slave_ip=${slave_ip:-192.168.1.101}
 
-mysql -u root -proot -e "CREATE USER slaveuser@$slave_ip IDENTIFIED WITH mysql_native_password BY 'pass';"
-mysql -u root -proot -e "grant replication slave on *.* to slaveuser@$slave_ip;"
-mysql -u root -proot -e "flush privileges;"
-mysql -u root -proot -e "show grants for slaveuser@$slave_ip;"
-mysql -u root -proot -e "show master status\G"
+read -p "Enter master address [192.168.1.100] : " master_ip
+master_ip=${master_ip:-192.168.1.100}
+
+read -p "Enter master_log_file address [mysql-bin.000002] : " master_log_file
+master_log_file=${master_log_file:-mysql-bin.000002}
+
+read -p "Enter master_log_pos address : " master_log_pos
+
+mysql -u root -proot -e "CHANGE MASTER TO MASTER_HOST='$master_ip', MASTER_USER='slaveuser', MASTER_PASSWORD='pass', MASTER_LOG_FILE='$master_log_file', MASTER_LOG_POS=$master_log_pos;"
+mysql -u root -proot -e "start slave;"
+mysql -u root -proot -e "show slave status\G"
+
+
+
 
