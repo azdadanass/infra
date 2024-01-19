d84de1e07170f1b4b557268d273a0ee92e93ab4a
Azdad Anass <a.azdad@3gcom-int.com>
2024-01-19 17:20:01 +0100

    ...

--- database/master/backup.sh
+++ database/master/backup.sh
@@ -8,7 +8,7 @@ backup_file=$backup_folder/$database`date +%Y-%m-%d_%H-%M`.7z
 
 echo "" 2> $error_log
 
-find $backup_folder -type f -name="$database*" -mtime +7 -delete
+find $backup_folder -type f -name "$database*" -mtime +7 -delete
 
 mysqldump \
  --defaults-extra-file=~/scripts/config.cnf \
