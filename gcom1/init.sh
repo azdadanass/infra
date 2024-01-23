f92932e0396f062ae35b65ad62c47efd06336a50
Azdad Anass <a.azdad@3gcom-int.com>
2024-01-23 14:15:34 +0100

    $UID

--- gcom1/init.sh
+++ gcom1/init.sh
@@ -16,6 +16,11 @@ sudo apt-get install sshfs
 ssh-keygen
 ssh-copy-id azdad@$files_server_ip
 
-echo "azdad@$files_server_ip:/home/azdad/gcom/files /home/azdad/files fuse.sshfs noauto,x-systemd.automount,_netdev,allow_other,IdentityFile=/home/azdad/.ssh/id_rsa,reconnect,follow_symlinks 0 0" | sudo tee -a /etc/fstab
+#echo "azdad@$files_server_ip:/home/azdad/gcom/files /home/azdad/files fuse.sshfs noauto,x-systemd.automount,_netdev,allow_other,IdentityFile=/home/azdad/.ssh/id_rsa,reconnect,follow_symlinks 0 0" | sudo tee -a /etc/fstab
+
+
+
+
+azdad@$files_server_ip:/home/azdad/gcom/files /home/azdad/files    fuse.sshfs  defaults,_netdev,allow_other,default_permissions,identityfile=/home/USER/.ssh/id_rsa,uid=$UID,gid=$UID    0   0
 
 echo please to reboot
