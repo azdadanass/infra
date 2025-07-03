0e84f638102dab2c3ef4345aac6c602bfac26bd9
Azdad Anass <a.azdad@3gcom-int.com>
2025-07-03 12:56:35 +0100

    update manager/init/install-docker.sh

--- manager/init/install-docker.sh
+++ manager/init/install-docker.sh
@@ -1,5 +1,24 @@
-sudo apt-get update
-sudo apt update && sudo apt install -y docker.io docker-buildx-plugin
+# Remove any existing Docker
+sudo apt remove docker.io
+
+# Set up Docker's official repository
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker with buildx
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
