93cf83eed41afd9de7323b203cf067a2d7da98ec
Azdad Anass <a.azdad@3gcom-int.com>
2025-07-15 09:53:04 +0100

    ...

--- new db/db1/scripts/install-nrpe.sh
+++ new db/db1/scripts/install-nrpe.sh
@@ -10,4 +10,8 @@ echo "command[check_mysql]=/usr/lib/nagios/plugins/check_mysql -H 127.0.0.1 -u n
 echo "command[check_mysql_threads]=/usr/lib/nagios/plugins/check_mysql_health --hostname=127.0.0.1 --username=nagios --password=nagios --mode=threads-connected --warning=30 --critical=50"  | sudo tee -a /etc/nagios/nrpe.cfg
 echo "command[check_mysql_slow]=/usr/lib/nagios/plugins/check_mysql_health --hostname=127.0.0.1 --username=nagios --password=nagios --mode=slow-queries --warning=10 --critical=20"  | sudo tee -a /etc/nagios/nrpe.cfg
 
+sudo mkdir -p /var/cache/nagios
+sudo chown nagios:nagios /var/cache/nagios
+sudo chmod 755 /var/cache/nagios
+
 sudo systemctl restart nagios-nrpe-server
